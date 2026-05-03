71f5d9972131509b1d771dad3955f4af7e0fe3f1
mkarvan <MK@Optimus525.local>
2026-05-03 12:55:47 -0700

    Set INFO as default log level so diagnostics show without RUST_LOG

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    env_logger::init();
+    env_logger::Builder::from_default_env()
+        .filter_level(log::LevelFilter::Info)
+        .init();
     scribebuddy::run()
 }
